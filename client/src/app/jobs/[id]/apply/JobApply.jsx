4e6666c6b9d1716fee063f14e28d68f310756b77
umayer10-AI <mdumayer10@gmail.com>
2026-06-07 19:12:05 +0600

    add

---
+++ client/src/app/jobs/[id]/apply/JobApply.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const JobApply = ({job}) => {
+
+    console.log(job)
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default JobApply;
--- client/src/app/jobs/[id]/apply/page.jsx
+++ client/src/app/jobs/[id]/apply/page.jsx
@@ -1,23 +1,35 @@
 import { auth } from '@/lib/auth';
+import { getJobsIdData } from '@/lib/jobData';
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 import React from 'react';
+import JobApply from './JobApply';
 
 const page = async ({params}) => {
 
     const {id} = await params
+    const job = await getJobsIdData(id);
 
     const session = await auth.api.getSession({
         headers: await headers()
     })
     const user = session?.user
+    console.log(user)
     if(!user){
         redirect(`/signin?redirect=jobs/${id}/apply`)
+    }
+    if(user.role !== 'seeker'){
+        return (
+            <div className='min-h-screen flex justify-center items-center'>
+                <h1 className='text-4xl font-bold'>Obly job seeker allowed</h1>
+            </div>
+        )
     }
 
     return (
         <div>
             apply
+            <JobApply job={job}></JobApply>
         </div>
     );
 };
